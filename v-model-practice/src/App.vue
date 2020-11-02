ecf347a90fe6524512f53e55c83f2d6d2e584084
eddie0329 <fdsa0329@naver.com>
2020-11-02 22:35:45 +0900

    :sparkles: Implement v-model practice

---
+++ v-model-practice/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <MyInput v-model="temp" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import MyInput from './components/MyInput';
+
+export default {
+  name: 'App',
+  data: () => ({
+    temp: 'HELLO'
+  }),
+  components: {
+    HelloWorld,
+    MyInput
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ v-model-practice/src/components/MyInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ value }}</h1>
+    <input type="text" :value="value" @input="onInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    onInput(event) {
+      this.$emit('input', event.target.value);
+    }
+  }
+};
+</script>
+
+<style></style>
