9b30cf175b6581397d80fb3981f2a9010fee49d8
eddie <fdsa0329@naver.com>
2020-11-08 12:54:21 +0900

    :sparkles: Implement vmodel practice

---
+++ v-model-practice01/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <CheckButton :value="isDone" @input="onInput" />
+  </div>
+</template>
+
+<script>
+import CheckButton from './components/CheckButton';
+
+export default {
+  name: 'App',
+  data: () => ({
+    isDone: false
+  }),
+  components: {
+    CheckButton
+  },
+  methods: {
+    onInput(e) {
+      console.log("HELLO");
+      console.log(e);
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ v-model-practice01/src/components/CheckButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <button @click="onClick">{{ value ? 'V' : ' ' }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Boolean,
+      default: false,
+    }
+  },
+  methods: {
+    onClick() {
+      this.$emit('input', !this.value);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
