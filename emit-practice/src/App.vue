db98f4e6fd1d2699f4b3c844f505d471b3bc845d
Eddie <40482274+eddie0329@users.noreply.github.com>
2021-03-19 18:10:53 +0900

    :sparkles: Implement promise pratice

---
+++ emit-practice/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <Child @click="onClick" />
+  </div>
+</template>
+
+<script>
+import Child from './components/Child';
+
+export default {
+  name: 'App',
+  components: { Child },
+  methods: {
+    async onClick(childResolve) {
+      console.log('HELLO');
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, 3000);
+      });
+      childResolve();
+      console.log('BYE');
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ emit-practice/src/components/Child.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1 v-if="!isLoaded">LOADING !!!!</h1>
+    <button @click="onClick">HELLO CLICK ME</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Child',
+  data: () => ({
+    isLoaded: false,
+  }),
+  methods: {
+    emit() {
+      return new Promise((resolve) => {
+        this.$emit('click', resolve);
+      });
+    },
+    onClick() {
+      this.emit();
+    },
+  },
+};
+</script>
+
+<style>
+</style>
