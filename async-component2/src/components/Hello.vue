3dec1fcc4c2eed0b55acef19a8018eb67f07af20
Eddie <40482274+eddie0329@users.noreply.github.com>
2021-03-19 18:11:05 +0900

    :sparkles: Implement new

---
+++ async-component2/src/components/Hello.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>HELLO {{ value }}</h1>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ async-component2/src/views/Asnyc.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>ASYNC</h1>
+    <async-component value="eddie"></async-component>
+  </div>
+</template>
+
+<script>
+import Hello from '../components/Hello';
+import Loading from '../components/Loading';
+
+const AsyncComponent = () => ({
+  component: new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(Hello);
+    }, 1000);
+  }),
+  loading: Loading,
+  delay: 0,
+  timeout: 3000,
+});
+
+export default {
+  components: { AsyncComponent },
+};
+</script>
+
+<style>
+</style>
