70022f25a2d46dbfbceccf77a82f79b6e655688f
Eddie <40482274+eddie0329@users.noreply.github.com>
2021-05-02 16:23:39 +0900

    :sparkles: Implement pop event

---
+++ route-go-back-test/src/views/About.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+/* eslint-disable no-unused-vars */
+export default {
+  name: 'About',
+  data: () => ({
+    popEvent: false,
+  }),
+  mounted() {
+    window.addEventListener('popstate', () => {
+      this.popEvent = true;
+    });
+    this.$router.beforeEach((to, from , next) => {
+      if (window.popEvent) {
+        return;
+      }
+    });
+  },
+};
+</script>
