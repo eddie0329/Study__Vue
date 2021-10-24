6c7a6a7ca7c5a2c150ef67549fcde08edf830a49
eddie0329 <fdsa0329@naver.com>
2021-10-24 21:20:46 +0900

    :fire: Remove unused

--- provider-test/src/components/Child2.vue
+++ provider-test/src/components/Child2.vue
@@ -1,19 +1,14 @@
 <template>
   <div>
-    <h1>COUNT CHILD2: {{ count }}</h1>
-    <h1>DOUBLE CHILD2: {{ double }}</h1>
-    <hr />
     <Child3 />
   </div>
 </template>
 
 <script>
 import Child3 from './Child3.vue';
-import { mapState, mapGetters } from '../utils/providerMixin';
 
 export default {
   components: { Child3 },
-  mixins: [mapState({ count: 'count' }), mapGetters({ double: 'double' })]
 };
 </script>
 
